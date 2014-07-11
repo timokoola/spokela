892612846b78af0cc7f8c672f9924a2e133c5f83
Timo Koola <timo.koola@codento.com>
2014-07-11 15:10:18 +0300

    spotify whatif

--- app/js/app.js
+++ app/js/app.js
@@ -10,34 +10,35 @@ var spotify_total = 4400;
 var radio_total = 58713;
 var radio_plays = 5530 + 659;
 var suomi = 5460000;
-updateValuesShown();
-updateRadio();
+var radio_total_audience = 0.0;
+
+updateAll();
 
 $('[data-slider]').on('change.fndtn.slider', function(){
-  updateValuesShown();
-  updateRadio();
+  updateAll();
 });
+
+function getSpotifyValue() {
+  var sp_value = $('#spotify_slider').attr('data-slider');
+  var sp_float = parseFloat(sp_value);
+  return sp_float;
+}
 
 // just because Javascript hates all humanity
 function updateValuesShown() {
-  var sp_value = $('#spotify_slider').attr('data-slider');
-  var sp_float = parseFloat(sp_value);
+  var sp_float = getSpotifyValue();
   $("#spotify_value").text(sp_float.toFixed(1));
   var sp_total_audience = sp_float * spotify_plays;
   $("#spotify_total_audience").text(sp_total_audience.toFixed(0));
   spotify_play_price = (spotify_total / sp_total_audience).toFixed(7);
   $("#spotify_price").text(spotify_play_price + "\u20AC");
-
-
-
-  
 }
 
 function updateRadio() {
   var radio_value = $('#radio_slider').attr('data-slider');
   var radio_float = parseFloat(radio_value);
   $("#radio_value").text(radio_float.toFixed(1));
-  var radio_total_audience = radio_float * radio_plays;
+  radio_total_audience = radio_float * radio_plays;
   $("#radio_total_audience").text(radio_total_audience.toFixed(0));
   $("#radio_aud").text(radio_float.toFixed(0));
   $("#radio_avg").text((radio_total_audience/suomi).toFixed(1));
@@ -48,4 +49,17 @@ function updateRadio() {
   $("#equil").text(equil.toFixed(2));
 
   $("#radio_price").text(price_per_play + "\u20AC");
+}
+
+function updateWhatIf() {
+	var sp_float = getSpotifyValue();
+	$("#spotify_supercrowd").text(radio_total_audience);
+	$("#spotify_whatif").text(((radio_total_audience * spotify_play_price) / sp_float ).toFixed(2));
+}
+
+
+function updateAll() {
+  updateValuesShown();
+  updateRadio();
+  updateWhatIf();
 }
